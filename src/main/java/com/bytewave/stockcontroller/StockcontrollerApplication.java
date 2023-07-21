f636c944c6319b8d1be18f17e7310c0360f786f5
bene <matheusbenediht@gmail.com>
2023-07-20 21:27:09 -0300

    First commit

---
+++ src/main/java/com/bytewave/stockcontroller/StockcontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.bytewave.stockcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockcontrollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytewave/stockcontroller/StockcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytewave.stockcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
